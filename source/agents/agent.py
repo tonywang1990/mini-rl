a7107b37af19a59455df6e1620b4d7d3dc158264
Tony Wang <tonywy@meta.com>
2022-12-04 12:03:51 -0600

    update file strcuturer

---
+++ source/agents/agent.py
@@ -0,0 +1,44 @@
+from typing import Dict, List, Optional, Set, Tuple
+
+import gym
+from gym.spaces import Space
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+
+class Agent(object):
+    def __init__(self, state_space: Space, action_space: Space, discount_rate: float, epsilon: float, learning_rate: float):
+        self._state_spacee = state_space
+        self._action_space = action_space
+        self._discount_rate = discount_rate
+        self._epsilon = epsilon
+        self._learning_rate = learning_rate
+
+    def sample_action(self, state: int):
+        raise NotImplementedError
+
+    def control(self, state: int, action: int, reward: float, new_state: int, terminal: bool):
+        raise NotImplementedError
+
+    def play_episode(self, env: gym.Env, learning: Optional[bool] = True, epsilon: Optional[float] = None, learning_rate: Optional[float] = None, video_path: Optional[str] = None):
+        if video_path is not None:
+            video = VideoRecorder(env, video_path)
+        state, info = env.reset()
+        terminal = False
+        steps = 0
+        if epsilon is not None:
+            self._epsilon = epsilon
+        if learning_rate is not None:
+            self._learning_rate = learning_rate
+        while not terminal:
+            action = self.sample_action(state)
+            new_state, reward, terminal, _, info = env.step(action)
+            if learning:
+                self.control(state, action, reward,
+                             new_state, terminal)
+            state = new_state
+            steps += 1
+            if video_path is not None:
+                video.capture_frame()
+        if video_path is not None:
+            video.close()
+        return reward, steps
