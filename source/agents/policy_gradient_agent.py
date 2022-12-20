255bc87ad1692ad52d955caa1a2511895f29daaf
Tony Wang <tonywy@meta.com>
2022-12-20 16:15:07 -0600

    policy gradient with baseline

--- source/agents/policy_gradient_agent.py
+++ source/agents/policy_gradient_agent.py
@@ -20,10 +20,11 @@ from torch.distributions import Categorical
 class DenseNet(nn.Module):
     def __init__(self, n_observations, n_actions):
         super(DenseNet, self).__init__()
-        self.layer1 = nn.Linear(n_observations, 128)
-        self.layer2 = nn.Linear(128, 128)
-        self.layer3 = nn.Linear(128, 128)
-        self.layer4 = nn.Linear(128, n_actions)
+        width = 128
+        self.layer1 = nn.Linear(n_observations, width)
+        self.layer2 = nn.Linear(width, width)
+        self.layer3 = nn.Linear(width, width)
+        self.layer4 = nn.Linear(width, n_actions)
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
